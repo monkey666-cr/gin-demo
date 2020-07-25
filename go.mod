69bc0fddbd31dcfba5ef67b2ebaac86d5b8d4a29
chenrun <17610780919@163.com>
2020-07-25 18:15:31 +0800

    解析配置文件

--- go.mod
+++ go.mod
@@ -2,4 +2,9 @@ module github.com/chenrun666/gin_demo
 
 go 1.14
 
-require github.com/gin-gonic/gin v1.6.3
+require (
+	github.com/fsnotify/fsnotify v1.4.7
+	github.com/gin-gonic/gin v1.6.3
+	github.com/spf13/pflag v1.0.5
+	github.com/spf13/viper v1.7.0
+)
--- main.go
+++ main.go
@@ -2,14 +2,29 @@ package main
 
 import (
 	"errors"
+	"github.com/chenrun666/gin_demo/config"
 	"github.com/chenrun666/gin_demo/router"
 	"github.com/gin-gonic/gin"
+	"github.com/spf13/pflag"
+	"github.com/spf13/viper"
 	"log"
 	"net/http"
 	"time"
 )
 
+var (
+	cfg = pflag.StringP("config", "c", "", "apiserver config file path")
+)
+
 func main() {
+	pflag.Parse()
+
+	if err := config.Init(*cfg); err != nil {
+		panic(err)
+	}
+
+	gin.SetMode(viper.GetString("runmode"))
+
 	g := gin.New()
 
 	var middlewares []gin.HandlerFunc
@@ -23,15 +38,14 @@ func main() {
 		log.Println("The router has been deployed successfully.")
 	}()
 
-	log.Printf("Start to listening the incoming requests on http address:%s\n", ":8080")
-	log.Printf(http.ListenAndServe(":8080", g).Error())
+	log.Printf("Start to listening the incoming requests on http address:%s\n", viper.GetString("addr"))
+	log.Printf(http.ListenAndServe(viper.GetString("addr"), g).Error())
 }
 
-
 func pingServer() error {
-	for i := 0; i < 10; i++ {
-		resp, err := http.Get("http://127.0.0.1:8080" + "/sd/health")
-		if err == nil && resp != nil && resp.StatusCode == 200{
+	for i := 0; i < viper.GetInt("max_ping_count"); i++ {
+		resp, err := http.Get(viper.GetString("url") + "/sd/health")
+		if err == nil && resp != nil && resp.StatusCode == 200 {
 			return nil
 		}
 

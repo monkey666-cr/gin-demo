873659fb001a5315dc3c87e95ba673d7615c27b2
chenrun <17610780919@163.com>
2020-07-27 20:25:52 +0800

    增加用户接口

--- router/router.go
+++ router/router.go
@@ -2,6 +2,7 @@ package router
 
 import (
 	"github.com/chenrun666/gin_demo/handler/sd"
+	"github.com/chenrun666/gin_demo/handler/user"
 	"github.com/chenrun666/gin_demo/router/middleware"
 	"github.com/gin-gonic/gin"
 	"net/http"
@@ -16,6 +17,12 @@ func Load(g *gin.Engine, mw ...gin.HandlerFunc) *gin.Engine {
 	g.NoRoute(func(c *gin.Context) {
 		c.String(http.StatusNotFound, "The incorrect API route")
 	})
+
+	// user
+	u := g.Group("/v1/user")
+	{
+		u.POST("", user.Create)
+	}
 
 	svcd := g.Group("/sd")
 	{
